f1fbc391aab6d28cf0b9598bf11c0bf0aa2532c1
ameliapham <ptt.huong.8895@gmail.com>
2025-12-31 00:24:05 +0100

    Update y position with sine wave based on time and x position to create smooth wave

--- src/script.tsx
+++ src/script.tsx
@@ -97,7 +97,8 @@ function animate(){
     // Update particles
     for (let i = 0; i < points; i++){
         const i3 = i * 3
-        particlesGeometry.attributes.position.array[i3 + 1] = Math.sin(elapsedTime + i) * 0.5 // Update y position with sine wave
+        const x = particlesGeometry.attributes.position.array[i3]
+        particlesGeometry.attributes.position.array[i3 + 1] = Math.sin(elapsedTime + x) * 0.5 //Update y position with sine wave based on time and x position
     }
     particlesGeometry.attributes.position.needsUpdate = true //Notify Three.js that positions have changed
     //particles.rotation.y = elapsedTime * 0.2
